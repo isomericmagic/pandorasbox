60b369ed97618110555facf57ff2ba16d234d833
isomericmagic <brian@isomericmagic.com>
2021-08-11 21:46:24 -0500

    Update pandoras_box.py
    minor osx updates

--- pandoras_box.py
+++ pandoras_box.py
@@ -6,11 +6,8 @@ from requests.exceptions import Timeout
 from requests.exceptions import MissingSchema
 import re
 from flask import Flask, request, render_template, Markup
-import subprocess
-
-def print_output(file_path_1):
-    final_response = "Your file path is: " + file_path_1
-    return final_response
+import appscript
+import os
 
 app = Flask(__name__)
 
@@ -19,9 +16,8 @@ def my_results_form():
     if request.method == 'POST':
         first_text = request.form['first_file']
         second_text = request.form['second_file']
-        #output = print_output(first_text + ' ... ' + second_text)
-        #return render_template('results.html', output = output)
-        subprocess.check_call("./test.sh %s %s" % (first_text, second_text), shell=True)
+        directory_path = os.getcwd()
+        appscript.app('Terminal').do_script('cd ' + directory_path + ';''./test.sh' + ' ' + first_text + ' ' + second_text)
         return render_template('results.html')
     elif request.method == 'GET':
         return render_template('index.html')
