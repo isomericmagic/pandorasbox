c279d8f5a16a374474e0d834a0c3036a7edd2ce9
isomericmagic <brian@isomericmagic.com>
2021-08-11 20:33:05 -0500

    initial commit

---
+++ static/app.js
@@ -0,0 +1,2 @@
+$( "p:contains('The following tags')" ).css( "font-weight", "bold" );
+$( "p:contains('Additional information')" ).css( "font-weight", "bold" );
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#The script demonstrates the use of variables entered at the command line.
+echo "First file path is $1"
+echo "Second file path is $2"
